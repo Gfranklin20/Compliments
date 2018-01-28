6004aec4695bf4a86526a8954fc51b99206ebdc1
Gregory Franklin <7gregboard@gmail.com>
2018-01-27 21:00:52 -0500

    UI Tutorial

---
+++ app/src/main/java/com/gregory_franklin/hackbrown/SendingActivity.java
@@ -0,0 +1,28 @@
+package com.gregory_franklin.hackbrown;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SendingActivity extends Activity {
+    private Button sendCompliment;
+    private EditText writeCompliment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sending);
+        sendCompliment = findViewById(R.id.sendCompliment);
+        writeCompliment = findViewById(R.id.writeCompliment);
+        sendCompliment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(sendCompliment, writeCompliment.getText(), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
