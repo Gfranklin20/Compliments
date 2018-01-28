696a8f4857ae9279bf6522f2c13fecb19b437f76
Gregory Franklin <7gregboard@gmail.com>
2018-01-28 01:59:36 -0500

    Updated version

--- app/src/main/java/com/gregory_franklin/hackbrown/Swipe.java
+++ app/src/main/java/com/gregory_franklin/hackbrown/Swipe.java
@@ -2,27 +2,33 @@ package com.gregory_franklin.hackbrown;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class HomeActivity extends AppCompatActivity {
+public class Swipe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
 
-        Button sendButton = findViewById(R.id.sendButton);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe);
+
+        ImageView contact1 = findViewById(R.id.contact1);
+        contact1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, Swipe.class);
+                Intent intent = new Intent(Swipe.this, SendingActivity.class);
                 startActivity(intent);
             }
 
         });
+
+
     }
 
 }
